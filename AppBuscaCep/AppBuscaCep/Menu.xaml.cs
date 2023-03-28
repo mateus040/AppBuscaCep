1556fa665507b08b5f477bcc7c0cdbf5f0343b77
mateus040 <mateusgabrielmoreno264@gmail.com>
2023-03-28 15:47:05 -0300

    adicionando estilso ao menu

--- AppBuscaCep/AppBuscaCep/Menu.xaml.cs
+++ AppBuscaCep/AppBuscaCep/Menu.xaml.cs
@@ -15,6 +15,8 @@ namespace AppBuscaCep
         public Menu()
         {
             InitializeComponent();
+
+            NavigationPage.SetHasNavigationBar(this, false);
         }
 
         private void Button_Clicked(object sender, EventArgs e)
