52b1c884ed61609a1cec90181703f87c92aca227
mateus040 <mateusgabrielmoreno264@gmail.com>
2023-04-04 14:15:59 -0300

    adicionando tela BuscarEnderecoPorCep

---
+++ AppBuscaCep/AppBuscaCep/View/BuscarEnderecoPorCep.xaml.cs
@@ -0,0 +1,55 @@
+﻿using AppBuscaCep.Model;
+using AppBuscaCep.Service;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppBuscaCep.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BuscarEnderecoPorCep : ContentPage
+    {
+        public BuscarEnderecoPorCep()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                simcarregando();
+
+                Endereco[] arr_end = { await DataService.GetEnderecoByCep(txt_cep.Text) };
+
+                lst_end.ItemsSource = arr_end;
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Erro!", ex.Message, "OK");
+            }
+            finally
+            {
+                naocarregando();
+            }
+        }
+
+        public void simcarregando()
+        {
+            carregando.IsEnabled = true;
+            carregando.IsRunning= true;
+        }
+        
+        public void naocarregando()
+        {
+            carregando.IsEnabled = false;
+            carregando.IsRunning= false;
+        }
+    }
+}
